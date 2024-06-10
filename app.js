949ea773296b3c48fd249ea6aa89558b7dbb8876
Saadaabdennour <saada.abdennour1@gmail.com>
2024-06-10 19:11:32 +0100

    LastTest

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/*", (req, res) => {
-    res.send('Hello World!');
+    res.send('SAADA Abdennour +++++ ESSBAA Adam');
 });
 
 // Error handling middleware

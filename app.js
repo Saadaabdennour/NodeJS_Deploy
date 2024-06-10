ea6a1239bfe78747566504d96b04bb5737a47ed6
Saadaabdennour <saada.abdennour1@gmail.com>
2024-06-10 15:13:32 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Middleware to log requests
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+app.get("/*", (req, res) => {
+    res.send('Hello World This is an Express App');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(PORT, () => console.log(`App Listening on port ${PORT}`));
